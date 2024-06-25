3d4fe22595c05eaaf4d896ca7ba9a921f8a9d50d
GroshkovR <rik1810huragan@gmail.com>
2024-06-26 10:16:01 +1100

    создал программу: Является ли первое число квадратом второго

---
+++ squareOrNot/squareOrNot.cs
@@ -0,0 +1,18 @@
+﻿// Является ли 1-е число , квадратом 2-го:
+// 25 и 5 --> yes
+
+
+Console.Write("Введите число которое на ваш взгляд является квадратом: ");
+int squareNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число которое на ваш взгляд является производным квадрата: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == squareNum / number)
+{
+    Console.WriteLine($"Число: {squareNum}, является квадратом числа: {number}.");
+}
+else
+{
+    Console.WriteLine($"Число: {squareNum}, не является квадратом числа: {number}.");
+}
