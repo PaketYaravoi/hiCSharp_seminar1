9d9a7636b06450d6043199dbe8566e646f9f4bc8
GroshkovR <rik1810huragan@gmail.com>
2024-06-27 16:47:42 +1100

    Написал программу: поиска четных чисел от 1 до N

---
+++ independentSem1/findAllEvenNum/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №8: Напишите программу , которая принимает число (N) а на выходе показывает все четные числа от 1 до N. 
+
+Console.WriteLine();
+
+int n = 0;
+
+
+Console.Write("Enter a valid integer: ");
+while (!int.TryParse(Console.ReadLine(), out n))
+{
+    Console.WriteLine("input eror!");
+    Console.Write("Please enter integer: ");
+}
+for (int showEvenNum = 2; showEvenNum < n; showEvenNum += 1)
+{
+    if (showEvenNum % 2 == 0)
+    {
+        Console.Write(showEvenNum + " ");
+    }
+}    
